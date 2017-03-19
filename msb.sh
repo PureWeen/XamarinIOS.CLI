252584251a4cf2df7aa0f530a246f512465b8284
Shane Neuville <ShaneN>
2017-03-19 13:37:25 -0600

    changed target to rebuild

--- msb.sh
+++ msb.sh
@@ -1,3 +1,3 @@
 export MSBuildSDKsPath=/usr/local/share/dotnet/sdk/1.0.1/Sdks/
 msbuild /t:restore NetStandard.Ref/NetStandard.Ref.csproj 
-msbuild XamarinIOS.CLI.sln /t:build /p:Configuration=release /p:Platform=iPhoneSimulator
+msbuild XamarinIOS.CLI.sln /t:rebuild /p:Configuration=release /p:Platform=iPhoneSimulator
